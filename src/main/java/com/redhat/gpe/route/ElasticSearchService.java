5efb82a14577c869872dbda9c8c6dbd3c791d65b
Charles Moulliard <ch007m@gmail.com>
2015-08-26 10:54:36 +0200

    Add exception handling. Add find_by_id method

--- src/main/java/com/redhat/gpe/route/ElasticSearchService.java
+++ src/main/java/com/redhat/gpe/route/ElasticSearchService.java
@@ -1,28 +1,47 @@
 package com.redhat.gpe.route;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.redhat.gpe.model.Blog;
 import org.apache.camel.*;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.PlainActionFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
 
 public class ElasticSearchService {
 
     final static Logger LOG = LoggerFactory.getLogger(ElasticSearchService.class);
-    
-    public IndexRequest addEntry(@Body Blog body,
-                                 @Header("indexname") String indexname,
-                                 @Header("indextype") String indextype,
-                                 @Header("id") String id) {
+
+    public IndexRequest add(@Body Blog body,
+                            @Header("indexname") String indexname,
+                            @Header("indextype") String indextype,
+                            @Header("id") String id) {
 
         String source = new Gson().toJson(body);
         LOG.info("Id : " + id + ", indexname : " + indexname + ", indextype : " + indextype);
         LOG.info("Source : " + source);
-        
+
         IndexRequest req = new IndexRequest(indexname, indextype, id);
         req.source(source);
         return req;
     }
-    
+
+    public String findById(@Header("id") String id) {
+        return id;
+    }
+
+    public String generateResponse(@Body PlainActionFuture future) throws Exception {
+        GetResponse response = (GetResponse) future.get();
+        String result = response.getSourceAsString().replace("\\", "");
+/*        ObjectMapper mapper = new ObjectMapper();
+        String output = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);*/
+        return result;
+    }
+
 }
--- src/main/java/com/redhat/gpe/route/RestToElasticDBRoute.java
+++ src/main/java/com/redhat/gpe/route/RestToElasticDBRoute.java
@@ -1,6 +1,5 @@
 package com.redhat.gpe.route;
 
-import com.google.gson.Gson;
 import com.redhat.gpe.model.Blog;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -9,9 +8,13 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.elasticsearch.ElasticsearchConfiguration;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.common.util.concurrent.BaseFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Future;
 
 public class RestToElasticDBRoute
         extends RouteBuilder {
@@ -26,26 +29,49 @@ public class RestToElasticDBRoute
         restConfiguration().component("jetty").host("0.0.0.0").port("9191").bindingMode(RestBindingMode.json);
 
         rest("/entries/")
-                .get("/{id}")
-                   .to("direct:findbyid")
+                .get("/{id}").consumes("application/json")
+                .to("direct:findbyid")
                 .put("/new/{id}").consumes("application/json").type(Blog.class)
-                   .to("direct:new");
+                .to("direct:new");
+
+        onException(org.elasticsearch.client.transport.NoNodeAvailableException.class)
+                .log("ElasticSearch server is not available - not started, network issue , ... !");
 
 
         from("direct:new")
                 .setHeader(ElasticsearchConfiguration.PARAM_INDEX_NAME).simple("{{indexname}}")
                 .setHeader(ElasticsearchConfiguration.PARAM_INDEX_TYPE).simple("{{indextype}}")
-                .setHeader(ElasticsearchConfiguration.OPERATION_INDEX).constant("INDEX")
-                
-                .bean(ElasticSearchService.class, "addEntry")
-                
+                //.setHeader(ElasticsearchConfiguration.OPERATION_INDEX).constant("INDEX")
+                .setHeader(ElasticsearchConfiguration.PARAM_OPERATION).constant(ElasticsearchConfiguration.OPERATION_INDEX)
+
+                .bean(ElasticSearchService.class, "add")
+
                 .to("elasticsearch://{{clustername}}?ip={{address}}")
                         //.to("elasticsearch://{{clustername}}?operation=INDEX&ip={{address}}&indexName={{indexname}}&indexType={{indextype}}")
                 .log("Response received : ${body}");
 
         from("direct:findbyid")
-                .to("elasticsearch://{{clustername}}?operation=GET_BY_ID&ip={{address}}&indexName={{indexname}}&indexType={{indextype}}")
-                .log("Response received : ${body}");
+                .log("Find By ID Service called !")
+                .setHeader(ElasticsearchConfiguration.PARAM_INDEX_NAME).simple("{{indexname}}")
+                .setHeader(ElasticsearchConfiguration.PARAM_INDEX_TYPE).simple("{{indextype}}")
+                .setHeader(ElasticsearchConfiguration.PARAM_OPERATION).constant(ElasticsearchConfiguration.OPERATION_GET_BY_ID)
+
+                .bean(ElasticSearchService.class, "findById")
+
+                .doTry()
+                  .to("elasticsearch://{{clustername}}?ip={{address}}")
+                  .bean(ElasticSearchService.class, "generateResponse")
+                .doCatch(org.elasticsearch.client.transport.NoNodeAvailableException.class)
+                  .process(new Processor() {
+                      @Override
+                      public void process(Exchange exchange) throws Exception {
+                          exchange.getIn().setBody("ElasticSearch server is not available, not started, network issue , ... !");
+                          exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "text/plain");
+                          exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+                      }
+                  })
+                .endDoTry();
+
 
     }
 }
