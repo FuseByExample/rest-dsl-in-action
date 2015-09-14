feb73b84d14c613db42e477467f4b43b9b2dbc6f
Charles Moulliard <ch007m@gmail.com>
2015-09-14 11:07:52 +0200

    Add workaround to reply to OPTIONS request. Move path to REST

--- routing/src/main/java/org/jboss/fuse/route/RestToServicesRoute.java
+++ routing/src/main/java/org/jboss/fuse/route/RestToServicesRoute.java
@@ -14,21 +14,37 @@ public class RestToServicesRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        restConfiguration().component("jetty").host("0.0.0.0").port("9191").enableCORS(true).bindingMode(RestBindingMode.json).dataFormatProperty("prettyPrint", "true");
+        restConfiguration().component("jetty").host("0.0.0.0").port("9191")
+                .enableCORS(true)
+                .bindingMode(RestBindingMode.json)
+                .dataFormatProperty("prettyPrint", "true");
 
-        rest("/blog/").id("rest-blog-service").produces("application/json").consumes("application/json")
+        rest("/blog/article").id("rest-blog-service").produces("application/json").consumes("application/json")
                 
-                .get("/article/search/id/{id}").id("rest-searchbyid")
+                .get("/search/id/{id}").id("rest-searchbyid")
                     .to("direct:searchById")
 
-                .get("/article/search/user/{user}").id("rest-searchbyuser").outTypeList(Blog.class)
+                .get("/search/user/{user}").id("rest-searchbyuser").outTypeList(Blog.class)
                    .to("direct:searchByUser")
 
-                .put("/article/{id}").id("rest-put-article").type(Blog.class)
+                .put("/{id}").id("rest-put-article").type(Blog.class)
                     .to("direct:add")
                 
-                .delete("/article/{id}").id("rest-deletearticle").type(Blog.class)
+                .delete("/{id}").id("rest-deletearticle").type(Blog.class)
                    .to("direct:remove");
+
+        /* 
+         * Workaround to support HTTP OPTIONS request required for Swagger API when CORS is enabled
+         * The Allow Header reports the operations supported.
+         * A new verb has been added - https://issues.apache.org/jira/browse/CAMEL-9129 but it will be available for 2.16 or 2.15.x
+         */
+        rest("/blog/article/")
+           .verb("options")
+           .route()
+           .setHeader("Access-Control-Allow-Origin", constant("*"))
+           .setHeader("Access-Control-Allow-Methods", constant("GET, HEAD, POST, PUT, DELETE, OPTIONS"))
+           .setHeader("Access-Control-Allow-Headers", constant("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"))
+           .setHeader("Allow", constant("GET, HEAD, POST, PUT, DELETE, OPTIONS"));
 
     }
 }
