9ff139e60d02b223a9fe10a2fcb41fb55a247019
Charles Moulliard <ch007m@gmail.com>
2015-09-15 18:38:37 +0200

    Fix issue. Now OPTIONS request works

--- routing/src/main/java/org/jboss/fuse/route/RestToServicesRoute.java
+++ routing/src/main/java/org/jboss/fuse/route/RestToServicesRoute.java
@@ -1,5 +1,8 @@
 package org.jboss.fuse.route;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.jboss.fuse.model.Blog;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -9,24 +12,25 @@ public class RestToServicesRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        restConfiguration().component("netty-http").host("0.0.0.0").port("9191")
+        restConfiguration().component("jetty").host("0.0.0.0").port("9191")
            .enableCORS(true)
            .bindingMode(RestBindingMode.json)
-           .dataFormatProperty("prettyPrint", "true")
-           .endpointProperty("matchOnUriPrefix", "true"); ;
+           .dataFormatProperty("prettyPrint", "true");
+        
+        // TO BE CHECKED --> .endpointProperty("matchOnUriPrefix", "true")
 
-        rest("/blog/").id("rest-blog-service").produces("application/json").consumes("application/json")
+        rest("/blog/article/").id("rest-blog-service").produces("application/json").consumes("application/json")
 
-           .get("/article/search/id/{id}").id("rest-searchbyid")
+           .get("search/id/{id}").id("rest-searchbyid")
                .to("direct:searchById")
 
-           .get("/article/search/user/{user}").id("rest-searchbyuser").outTypeList(Blog.class)
+           .get("search/user/{user}").id("rest-searchbyuser").outTypeList(Blog.class)
                 .to("direct:searchByUser")
 
-           .put("/article/").id("rest-put-article").type(Blog.class)
+           .put().id("rest-put-article").type(Blog.class)
                 .to("direct:add")
 
-           .delete("/article/{id}").id("rest-deletearticle").type(Blog.class)
+           .delete("{id}").id("rest-deletearticle").type(Blog.class)
                .to("direct:remove");
 
            /*
@@ -34,13 +38,11 @@ public class RestToServicesRoute extends RouteBuilder {
             * The Allow Header reports the operations supported.
             * A new verb has been added - https://issues.apache.org/jira/browse/CAMEL-9129 but it will be available for 2.16 or 2.15.x
             */
-           rest("/").id("rest-options")
-             .verb("options","/blog/article")
-             .route()
+           rest("/blog/article").id("rest-options")
+             .verb("options").route()
              .setHeader("Access-Control-Allow-Origin", constant("*"))
              .setHeader("Access-Control-Allow-Methods", constant("GET, HEAD, POST, PUT, DELETE, OPTIONS"))
              .setHeader("Access-Control-Allow-Headers", constant("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"))
              .setHeader("Allow", constant("GET, HEAD, POST, PUT, DELETE, OPTIONS"));
-
     }
 }
