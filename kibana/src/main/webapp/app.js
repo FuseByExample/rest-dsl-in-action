88fac9b751b3d839667af67e351e70253f6a832d
Charles Moulliard <ch007m@gmail.com>
2015-11-09 10:01:18 +0100

    Add nodes package config file and app.js server to run the server using node app.js

---
+++ kibana/src/main/webapp/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+// Enable CORS for all requests
+app.use(cors());
+
+// allow serving of static files from the public directory
+app.use(express.static(__dirname));
+
+var port = 8001;
+var host = '0.0.0.0';
+var server = app.listen(port, host, function() {
+  console.log("App started at: " + new Date() + " on port: " + port); 
+});
